7374e40a1e4c3d760373ed79332ca6170ec722b5
Arnold <arnoldnderitu@gmail.com>
2023-05-18 21:11:56 +0300

    initialize apifairy extension

--- api/app.py
+++ api/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Config
+from apifairy import APIFairy
+
+apifairy = APIFairy()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # extensions
+    apifairy.init_app(app)
 
     return app
