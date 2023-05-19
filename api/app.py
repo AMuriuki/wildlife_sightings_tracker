3ef39297f9a85960f57f7646349b6aeba0b94044
Arnold <arnoldnderitu@gmail.com>
2023-05-19 09:05:45 +0300

    initialize DB dependencies

--- api/app.py
+++ api/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from config import Config
 from apifairy import APIFairy
+from alchemical.flask import Alchemical
+from flask_migrate import Migrate
 
 apifairy = APIFairy()
+migrate = Migrate()
+db = Alchemical()
 
 
 def create_app(config_class=Config):
@@ -10,6 +14,12 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     # extensions
+    db.init_app(app)
+    migrate.init_app(app)
     apifairy.init_app(app)
+
+    @app.route("/")
+    def index():
+        return redirect(url_for("apifairy.docs"))
 
     return app
