a8ead15b8f30afe113cd6254fddd22f6f6b5fa20
Arnold <arnoldnderitu@gmail.com>
2023-05-19 09:10:49 +0300

    configure DB options

--- config.py
+++ config.py
@@ -5,5 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    pass
+    # DB configuration
+    ALCHEMICAL_DATABASE_URL = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "wildlife_sightings.sql")
+
+    APIFAIRY_TITLE = "Wildlife Sightings Tracker API"
+    APIVERSION = "1.0"
+    APIFAIRY_UI = os.environ.get("DOCS_UI", "elements")
