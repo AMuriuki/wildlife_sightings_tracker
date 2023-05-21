0a6efba2c3237375a4b4015cac3ab2e3b765d05c
Arnold <arnoldnderitu@gmail.com>
2023-05-21 20:49:41 +0300

    display sightings

--- frontend/src/Pages/SightingsPage.js
+++ frontend/src/Pages/SightingsPage.js
@@ -5,50 +5,51 @@ import TimeAgo from "../components/TimeAgo";
 import { useApi } from "../context/ApiProvider";
 
 export default function SightingsPage() {
-    const [sightings, setSightings] = useState({ data: [] });
+    const [sightings, setSightings] = useState([]);
 
-    const api = useApi()
+    const api = useApi();
 
-    let url = '/sightings'
+    let url = '/sightings';
 
     useEffect(() => {
         (async () => {
             const response = await api.get(url);
             if (response.ok) {
-                setSightings(response.body.data)
+                setSightings(response.body.data);
             } else {
-                setSightings(null);
+                setSightings([]);
             }
         })();
-    }, [api, url])
+    }, [api, url]);
 
-    // create a unique list of species
-    const speciesList = sightings.data.reduce((uniqueSpecies, sighting) => {
+    const columnNames = ["Species", "Last Seen"];
+
+    const speciesList = sightings.reduce((uniqueSpecies, sighting) => {
         const { id, title } = sighting.species;
         if (!uniqueSpecies.find(species => species.id === id)) {
             uniqueSpecies.push({ id, title });
         }
-        return uniqueSpecies
+        return uniqueSpecies;
     }, []);
-
-    const columnNames = ["Species", "Last Seen"]
 
     return (
         <Body>
             <Card
                 heading="Sightings"
-                description="Select animal to view its sightings or add a new sighting"
+                description="Select an animal to view its sightings or add a new sighting"
                 tableData={speciesList.map(species => {
-                    const speciesSightings = sightings.data.filter(sighting => sighting.species.id === species.id);
-                    const lastSeen = speciesSightings[0].last_seen;
+                    const speciesSightings = sightings.filter(
+                        sighting => sighting.species.id === species.id
+                    );
+                    const lastSeen = speciesSightings[0]?.last_seen;
                     return {
                         species: species.title,
                         last_seen: <TimeAgo isoDate={lastSeen} />
-                    }
+                    };
                 })}
                 columnNames={columnNames}
                 currentPage="Sightings"
             />
         </Body>
-    )
+    );
 }
